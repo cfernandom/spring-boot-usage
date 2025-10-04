5aa168e49aa262310c7aa144a7c9cf31e6b681eb
cfernandom <cristian_21_97@hotmail.com>
2025-10-04 18:31:53 +0200

    configurar puerto de la aplicacion

---
+++ src/main/java/com/uptc/frw/ferialibroweb/FeriaLibroWebApplication.java
@@ -0,0 +1,13 @@
+package com.uptc.frw.ferialibroweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeriaLibroWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeriaLibroWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uptc/frw/ferialibroweb/FeriaLibroWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uptc.frw.ferialibroweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeriaLibroWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
